f7d58baea80fd0438bd7807ad6a48ab8ba19bc3a
Derpius <49565664+Derpius@users.noreply.github.com>
2022-09-07 04:08:45 +0100

    Initial commit

---
+++ distex.py
@@ -0,0 +1,47 @@
+from sympy import preview
+import discord
+from discord.ext import commands
+from io import BytesIO
+import re
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+latexPattern = re.compile(r"```latex\n.*?```", re.S)
+
+intents = discord.Intents.default()
+intents.messages = True
+
+client = commands.Bot(command_prefix=",", help_command=None, intents=intents)
+
+@client.event
+async def on_message(msg: discord.Message):
+	'''Convert LaTeX input to an image and reply with it'''
+	if msg.author.id == client.user.id: return
+
+	files = []
+	for match in latexPattern.finditer(msg.content):
+		buf = BytesIO()
+
+		try:
+			preview(
+				f"$${match.group(0)[9:-3]}$$",
+				output="png", viewer="BytesIO", outputbuffer=buf,
+				euler=False,
+				dvioptions=["-D", "160", "-bg", "Transparent", "-fg", "rgb 0.8666666666666667 0.8705882352941177 0.8745098039215686"]
+			)
+		except RuntimeError:
+			await msg.reply("Invalid syntax")
+			return
+
+		buf.seek(0)
+		files.append(discord.File(fp=buf, filename="latex.png"))
+
+	if files: await msg.reply(files=files)
+
+token = os.getenv("DISTEX_TOKEN")
+if token is None:
+	print("DISTEX_TOKEN is not set")
+	exit(-1)
+client.run(token)
